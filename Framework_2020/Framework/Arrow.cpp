4af8430cd442517adc71c8de257dbdf845a9538e
XxCtrlZxX <harryjung1001@gmail.com>
2020-12-01 17:07:25 +0900

    Arrow move1

--- Framework_2020/Framework/Arrow.cpp
+++ Framework_2020/Framework/Arrow.cpp
@@ -1,8 +1,9 @@
 #include "stdafx.h"
 #include "Arrow.h"
+#include "InputManager.h"
 
 Arrow::Arrow() 
-	: GameObject(L"resources/image/arrow_1.png")
+	: GameObject(L"resources/image/arrow_1.png"), state(eArrow::Right), posVec(Vector2(0, -1))
 {
 }
 
@@ -17,5 +18,22 @@ void Arrow::Update()
 
 void Arrow::Move()
 {
-	
+	if (InputManager::GetKeyDown(VK_UP))
+	{
+		posVec.y += posVec.y < 0 ? 1 : 0;
+	}
+	if (InputManager::GetKeyDown(VK_DOWN))
+	{
+		posVec.y -= posVec.y > limitPos.y ? 1 : 0;
+	}
+	if (InputManager::GetKeyDown(VK_RIGHT))
+	{
+		posVec.x += posVec.x < limitPos.x ? 1 : 0;
+	}
+	if (InputManager::GetKeyDown(VK_LEFT))
+	{
+		posVec.x -= posVec.x > 0 ? 1 : 0;
+	}
+
+	transform->SetPosition(posVec * 75 + offset);
 }
--- Framework_2020/Framework/Arrow.h
+++ Framework_2020/Framework/Arrow.h
@@ -1,13 +1,22 @@
 #pragma once
 #include "GameObject.h"
+
+enum class eArrow {
+	Right, Down, RD, RU
+};
+
 class Arrow : public GameObject
 {
 public:
 	Arrow();
 	~Arrow();
 
-	virtual void Update();
+	eArrow state;
+	Vector2 posVec;
+	const Vector2 limitPos = Vector2(3, -4);
+	const Vector2 offset = Vector2(-150, 150);
 
+	virtual void Update();
 	void Move();
 };
 
